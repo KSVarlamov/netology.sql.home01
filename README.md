0175b36ede43fc6063783f213b78b56d8cfd8418
KSVarlamov <varlamov.ks@mail.ru>
2023-05-13 23:49:23 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Задача "Таблица пользователей"
+
+Решение задачи [отсюда](https://github.com/netology-code/jd-homeworks/blob/master/sql-basic/task/README.md)
+
+## Общие сведения
+
+* ``create-table.sql`` - создаёт БД
+* 
+ 
+
---
+++ src/create-table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS public.persons
+(
+    name character varying(255) NOT NULL,
+    surname character varying(255) NOT NULL,
+    age integer NOT NULL,
+    phone_number character varying(20),
+    city_of_living character varying(255),
+    CONSTRAINT persons_pkey PRIMARY KEY (name, surname, age)
+)
